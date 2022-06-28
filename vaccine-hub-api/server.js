15902641cee4c73790d9f3a008c7c1522644e4d0
Enzo Falone <enzofalone123@gmail.com>
2022-06-28 13:46:36 -0400

    Implemented error handling class and use

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const { NotFoundError } = require('./utils/errors');
 
 //init express
 const app = express()
 
 //enables cross-origin resource sharing for all origins
 app.use(cors());
+
 // parse incoming request bodies with JSON payloads
 app.use(express.json());
+
 //log request info
 app.use(morgan('tiny'));
 
+// generic error handling (404)
+app.use((req, res, next) => {
+    return next(new NotFoundError());
+})
+
+// generic error handling: Unhandled errors (500)
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    const message = err.message
+
+    return res.status(status).json({
+        error: { message, status }
+    })
+})
+
+//initialize server listen at PORT
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
